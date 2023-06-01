cb43c115d3deffcc518720f75266f8ec2e6557b2
admin <admin@DESKTOP-M40D7GV>
2023-06-01 22:29:25 +0300

    Add project files.

---
+++ laba5/Form1.Designer.cs
@@ -0,0 +1,332 @@
+﻿
+namespace laba5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(404, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Wolf";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(37, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Animal";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(736, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 60);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Create Animal";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(736, 207);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 61);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Add Wolf";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(948, 620);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(103, 61);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Cancel";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(178, 119);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 17;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(77, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 19);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Species";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(77, 162);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 19);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Weight";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(77, 249);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(174, 19);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Daily maintenance cost";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(77, 207);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 19);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Age";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(178, 162);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 22;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(261, 246);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 23;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(178, 204);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 22);
+            this.textBox6.TabIndex = 24;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(572, 119);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 22);
+            this.textBox7.TabIndex = 25;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(439, 162);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(121, 19);
+            this.label7.TabIndex = 26;
+            this.label7.Text = "Place of habitat";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(439, 122);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(49, 19);
+            this.label8.TabIndex = 27;
+            this.label8.Text = "Breed";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(572, 162);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 22);
+            this.textBox8.TabIndex = 28;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Location = new System.Drawing.Point(474, 343);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(92, 19);
+            this.label10.TabIndex = 57;
+            this.label10.Text = "Sorted Array";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(572, 343);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(284, 250);
+            this.textBox2.TabIndex = 56;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(45, 340);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(47, 19);
+            this.label9.TabIndex = 55;
+            this.label9.Text = "Lab 6";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(103, 343);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(284, 250);
+            this.textBox1.TabIndex = 54;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(948, 353);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(103, 61);
+            this.button4.TabIndex = 58;
+            this.button4.Text = "Clone";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button5.Location = new System.Drawing.Point(948, 459);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(103, 61);
+            this.button5.TabIndex = 59;
+            this.button5.Text = "Sort";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1140, 799);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Animal";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ laba5/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laba5
+{
+    public partial class Form1 : Form
+    {
+        List<IAnimal> animals;
+
+        public Form1()
+        {
+            InitializeComponent();
+            animals = new List<IAnimal>();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                IAnimal animal = new Animal("Wolf", 50, 3, 60);
+                animals.Add(animal);
+
+                textBox3.Text = animal.Type;
+                textBox4.Text = animal.Weight.ToString();
+                textBox6.Text = animal.Age.ToString();
+                textBox5.Text = animal.Price.ToString();
+
+                textBox1.AppendText($"Added Animal: {animal.Type} - {animal.Weight}kg, {animal.Age} years, ${animal.Price}{Environment.NewLine}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception occurred: " + ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                IAnimal wolf = new Wolf("Wolf", 50, 3, 60, "Gray wolf", "Europe");
+                animals.Add(wolf);
+
+                textBox7.Text = ((Wolf)wolf).Poroda;
+                textBox8.Text = ((Wolf)wolf).Habitat;
+
+                textBox1.AppendText($"Added Wolf: {((Wolf)wolf).Poroda} - {((Wolf)wolf).Habitat}{Environment.NewLine}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception occurred: " + ex.Message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (animals == null || animals.Count == 0)
+                {
+                    MessageBox.Show("No animals to clone.");
+                    return;
+                }
+
+                int halfCount = animals.Count / 2;
+                for (int i = 0; i < halfCount; i++)
+                {
+                    IAnimal clone = (IAnimal)animals[i].Clone();
+                    animals.Add(clone);
+                    textBox1.AppendText($"Cloned: {clone.Type}{Environment.NewLine}");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception occurred: " + ex.Message);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (animals == null || animals.Count == 0)
+                {
+                    MessageBox.Show("No animals to sort.");
+                    return;
+                }
+
+                animals.Sort();
+
+                textBox1.AppendText($"Sorted Animals:{Environment.NewLine}");
+                foreach (IAnimal animal in animals)
+                {
+                    textBox1.AppendText($"{animal.Type} - {animal.Weight}kg, {animal.Age} years, ${animal.Price}{Environment.NewLine}");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception occurred: " + ex.Message);
+            }
+        }
+    }
+
+    public interface IAnimal : IComparable<IAnimal>, ICloneable
+    {
+        string Type { get; set; }
+        int Weight { get; set; }
+        int Age { get; set; }
+        int Price { get; set; }
+    }
+
+    public class Animal : IAnimal
+    {
+        public string Type { get; set; }
+        public int Weight { get; set; }
+        public int Age { get; set; }
+        public int Price { get; set; }
+
+        public Animal(string type, int weight, int age, int price)
+        {
+            Type = type;
+            Weight = weight;
+            Age = age;
+            Price = price;
+        }
+
+        public int CompareTo(IAnimal other)
+        {
+            if (other == null)
+                return 1;
+
+            return Type.CompareTo(other.Type);
+        }
+
+        public object Clone()
+        {
+            return new Animal(Type, Weight, Age, Price);
+        }
+    }
+
+    public class Wolf : Animal
+    {
+        public string Poroda { get; set; }
+        public string Habitat { get; set; }
+
+        public Wolf(string type, int weight, int age, int price, string poroda, string habitat)
+            : base(type, weight, age, price)
+        {
+            Poroda = poroda;
+            Habitat = habitat;
+        }
+
+        public new int CompareTo(IAnimal other)
+        {
+            if (other == null)
+                return 1;
+
+            if (other is Wolf otherWolf)
+                return Poroda.CompareTo(otherWolf.Poroda);
+
+            return base.CompareTo(other);
+        }
+
+        public new object Clone()
+        {
+            return new Wolf(Type, Weight, Age, Price, Poroda, Habitat);
+        }
+    }
+}
+
